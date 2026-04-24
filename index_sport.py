55cc271ef13b3e4e81d9c1aeee4bb6577851ca42
Konrad Kościecha <kondzio3135@gmail.com>
2026-04-24 20:59:20 +0200

    Upgrade score in docs value in index_sport.py

--- index_sport.py
+++ index_sport.py
@@ -7,12 +7,10 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 # Zamiast wczytywania plików:
 docs = [
-    "Drużyna A wygrała 2:1 z Drużyną B. Ostatnie 5 meczów: W W L D W. Kondycja: dobra, brak kontuzji.",
-    "Drużyna B przegrała 1:2 z Drużyną A. Ostatnie 5 meczów: L L W D L. Kondycja: średnia, 2 kontuzje kluczowych zawodników.",
-    "Drużyna A zremisowała 0:0 z Drużyną C. Forma stabilna, wysoka wydolność fizyczna.",
-    "Drużyna B wygrała 3:0 z Drużyną C. Poprawa formy, lepsza defensywa.",
-    "Drużyna A wygrała 4:2 z Drużyną D. Atak w świetnej formie.",
-    "Drużyna B przegrała 0:1 z Drużyną D. Problemy w ofensywie, niska skuteczność."
+    "Drużyna B wygrała 3:0 z Drużyną C. Poprawa formy, lepsza defensywa. Drużyna B przegrała 1:2 z Drużyną A. Ostatnie 5 meczów: L L W D L. Kondycja: średnia, 2 kontuzje kluczowych zawodników.",
+    "Drużyna A wygrała 4:2 z Drużyną D. Atak w świetnej formie. Drużyna A wygrała 2:1 z Drużyną B. Ostatnie 5 meczów: W W L D W. Kondycja: dobra, brak kontuzji. Drużyna A zremisowała 0:0 z Drużyną C. Forma stabilna, wysoka wydolność fizyczna.",
+    "Drużyna B przegrała 0:1 z Drużyną D. Problemy w ofensywie, niska skuteczność. Drużyna A wygrała 4:2 z Drużyną D. Atak w świetnej formie. ",
+    "Drużyna B wygrała 3:0 z Drużyną C. Poprawa formy, lepsza defensywa. Drużyna A zremisowała 0:0 z Drużyną C. Forma stabilna, wysoka wydolność fizyczna."
 ]
 
 embeddings = model.encode(docs)
